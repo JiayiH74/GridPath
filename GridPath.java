f032006fdc787da107ce39a59b2803f7f53fab6e
JiayiH74 <jiayih12@nycstudents.net>
2024-11-12 16:40:30 +0000

    updated

--- GridPath.java
+++ GridPath.java
@@ -1,28 +1,45 @@
 public class GridPath {
-    /** Initialized in the constructor with distinct values that never change */
+   
     private int[][] grid;
+    public GridPath (int[][]values)
+    {
+        grid = values;
+    }
+   
+    public Location getNextLoc(int row, int col)           //depending on if there are more neighbors below or to the right, it prints based on the amount
+    {
+        int below = Integer.MAX_VALUE;
+        if(row < grid.length - 1) below = grid[row + 1][col];   
+        int right = Integer.MAX_VALUE;
+        if(col < grid[0].length - 1) right = grid[row][col + 1];    //if there are more neighbors to the right than there are below, it reutns the neighbor to the right
+        if (below < right) return new Location(row + 1, col);
+        else return new Location(row, col + 1);                     //else, it returns the neightbor below
+    }
 
-    /**
-     * Returns the Location representing a neighbor of the grid element at row and
-     * col,
-     * as described in part (a)
-     * Preconditions: row is a valid row index and col is a valid column index in
-     * grid.
-     * row and col do not specify the element in the last row and last column of
-     * grid.
-     */
-    public Location getNextLoc(int row, int col) {
-        /* to be implemented in part (a) */ }
+    public int sumPath(int row, int col) 
+    {
+        int sum = grid[row][col];
+        while(row < grid.length - 1 || col < grid[0].length - 1)
+        {
+            Location loc = getNextLoc(row, col);
+            row = loc.getRow();
+            col = loc.getCol();
+            sum += grid[row][col];
+        }
+        return sum;
+    }
 
-    /**
-     * Computes and returns the sum of all values on a path through grid, as
-     * described in
-     * part (b)
-     * Preconditions: row is a valid row index and col is a valid column index in
-     * grid.
-     * row and col do not specify the element in the last row and last column of
-     * grid.
-     */
-    public int sumPath(int row, int col) {
-        /* to be implemented in part (b) */ }
+    public String toString()
+    {
+        String s = "";
+        for (int row = 0; row < grid.length; row++) // traversing each row
+        {
+            for (int col = 0; col < grid[0].length; col++) //traversing each column, incrementing
+            {
+                s += grid[row][col] + " "; // access the array at the column and row
+            }
+            s += "\n"; //after each column is checked for each row it breaks a new line
+        }
+        return s;
+    }
 }
