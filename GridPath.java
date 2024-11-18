b2a91113b888997c8831cd038f1ecaedce81ec6a
JiayiH74 <jiayih12@nycstudents.net>
2024-11-18 16:33:51 +0000

    final

--- GridPath.java
+++ GridPath.java
@@ -11,12 +11,12 @@ public class GridPath {
         int below = Integer.MAX_VALUE;
         if(row < grid.length - 1) below = grid[row + 1][col];   
         int right = Integer.MAX_VALUE;
-        if(col < grid[0].length - 1) right = grid[row][col + 1];    //if there are more neighbors to the right than there are below, it reutns the neighbor to the right
+        if(col < grid[0].length - 1) right = grid[row][col + 1];    //if there are more neighbors to the right than there are below, it returns the neighbor to the right
         if (below < right) return new Location(row + 1, col);
         else return new Location(row, col + 1);                     //else, it returns the neightbor below
     }
 
-    public int sumPath(int row, int col) 
+    public int sumPath(int row, int col) //compares the value to the right and bottom of chosen value and chooses the smaller one
     {
         int sum = grid[row][col];
         while(row < grid.length - 1 || col < grid[0].length - 1)
